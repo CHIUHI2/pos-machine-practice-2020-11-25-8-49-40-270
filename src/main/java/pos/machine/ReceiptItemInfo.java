e1cd051fbd269eb4d45313ad997fbbf07b719bda
Hins Chiu <chiuhi2@oocl.com>
2020-11-26 09:54:31 +0800

    [POS][HinsChiu]implementation:remove useless variable in ReceiptItemInfo

--- src/main/java/pos/machine/ReceiptItemInfo.java
+++ src/main/java/pos/machine/ReceiptItemInfo.java
@@ -2,13 +2,10 @@ package pos.machine;
 
 public class ReceiptItemInfo extends ItemInfo{
     private final int quantity;
-    private final int subTotal;
-
 
     public ReceiptItemInfo(String barcode, String name, int unitPrice, int quantity) {
         super(barcode, name, unitPrice);
         this.quantity = quantity;
-        this.subTotal = 0;
     }
 
     public int getQuantity() { return quantity; }
